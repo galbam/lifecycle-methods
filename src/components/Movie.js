63abe5706b57282d3bafc8add0e8d7f6e9d02997
Gonzalo <gonzalo.alba@live.com>
2019-08-05 12:19:16 +0200

    Initial commit

---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+
+class Movie extends React.Component {
+
+  componentDidMount() {
+    console.log("<Movie /> DID MOUNT");
+  }
+
+  componentDidUpdate() {
+    console.log("<Movie /> DID UPDATE");
+
+  }
+
+  componentWillUnmount() {
+    console.log("<Movie /> WILL UNMOUNT");
+
+  }
+
+  render() {
+    const { title, director, hasOscars, IMDbRating } = this.props;
+    return (
+      <Fragment>
+        <h2>{title}</h2>
+        <p>directed by {director}</p>
+        <p>IMDB rating: {IMDbRating}</p>
+        {hasOscars && <h4>Award-winning movie!</h4>}
+        <button onClick={this.props.deleteMovie}>delete</button>
+      </Fragment>
+    );
+  }
+}
+
+export default Movie;
